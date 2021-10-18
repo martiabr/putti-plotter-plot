fd243963a852b3e299da0413dfb1f6e19f1141e7
martiabr <martin.abrandt@outlook.com>
2021-10-18 21:55:11 +0200

    Add polygons to circle drawing

--- circles/sketch_circles.py
+++ circles/sketch_circles.py
@@ -1,11 +1,7 @@
-import vsketch
 import numpy as np
-import numpy.random as random
-import scipy.stats as stats
-
-def get_truncated_normal(mean=0, std=1, lower=0, upper=10):
-    return stats.truncnorm(
-        (lower - mean) / std, (upper - mean) / std, loc=mean, scale=std).rvs()
+from numpy.random import uniform, randint
+import vsketch
+import plotter_util as plutil
 
 class CirclesSketch(vsketch.SketchClass):
     # Sketch parameters:
@@ -16,26 +12,40 @@ class CirclesSketch(vsketch.SketchClass):
     r_step_std = vsketch.Param(0.5, min_value=0)
     min_steps = vsketch.Param(8, min_value=0)
     max_steps = vsketch.Param(16, min_value=0)
+    padding = vsketch.Param(3.0, min_value=0)
+    shape_type = vsketch.Param('circle', choices=['circle', 'polygon'])
+    polygon_sides = vsketch.Param(3, min_value=3)
+    
     width = 21
     height = 29.7
-    padding = vsketch.Param(3.0, min_value=0)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a4", landscape=False)
         vsk.scale("cm")
         
         for i in range(self.N_circles):
-            radius = get_truncated_normal(self.r_mean, self.r_std, 0.01, 10.0)
-            x = np.random.uniform(self.padding, self.width - self.padding)
-            y = np.random.uniform(self.padding, self.height - self.padding)
-            n = np.random.randint(self.min_steps, self.max_steps)
-            k = get_truncated_normal(self.r_step_mean, self.r_step_std, 8e-2, 0.5)
+            x = uniform(self.padding, self.width - self.padding)
+            y = uniform(self.padding, self.height - self.padding)
+            
+            radius = plutil.get_truncated_normal(self.r_mean, self.r_std, 0.01, 10.0)
+            k = plutil.get_truncated_normal(self.r_step_mean, self.r_step_std, 8e-2, 0.5)
+            phase = uniform(-np.pi, np.pi)
+            
+            if self.min_steps == self.max_steps:
+                n = self.min_steps
+            else:
+                n = randint(self.min_steps, self.max_steps)
+
             for j in range(n):
-                vsk.circle(x, y, radius + k*j, mode="radius")
+                if self.shape_type == 'circle':
+                    vsk.circle(x, y, radius + k*j, mode="radius")
+                elif self.shape_type == 'polygon':
+                    angles = np.linspace(0, 2 * np.pi, self.polygon_sides + 1)
+                    vsk.polygon(x + (radius + k*j) * np.cos(angles + phase), y + (radius + k*j) * np.sin(angles + phase))
+                    
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
-
 
 if __name__ == "__main__":
     CirclesSketch.display()
