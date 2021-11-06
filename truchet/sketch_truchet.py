f1fe9b36a6ae4e20fee589ee7139dbb8f79bb72d
martiabr <martin.abrandt@outlook.com>
2021-11-06 21:29:38 +0100

    Add removal of simple circle patterns

--- truchet/sketch_truchet.py
+++ truchet/sketch_truchet.py
@@ -15,8 +15,12 @@ from enum import Enum
 # - experiment with own
 
 # TODO:
-# - make it possible to choose distirbution of tiles explicitly
+# - make it possible to choose distirbution of tiles explicitly possibly with string to np array
 # - possibly shadows
+# - optional 4-way connection circle point
+# - make connect mode work with many mode
+# - optional kill of circle? and possibly other simple loops
+# - merge N_fill and knot_N
 
 
 class TruchetSketch(vsketch.SketchClass):
@@ -30,11 +34,12 @@ class TruchetSketch(vsketch.SketchClass):
     tile_sets = Enum('TruchetTileSet', 'circles triangles diagonals knot')
     tile_set = vsketch.Param(tile_sets.knot.name, choices=[tile_set.name for tile_set in tile_sets])
     
-    knot_mode = vsketch.Param('only_center', choices=['only_center', 'only_outer', 'many'])
+    knot_mode = vsketch.Param('outline', choices=['center', 'outline', 'many'])
     knot_N = vsketch.Param(4)
-    knot_thickness = vsketch.Param(0.25)
-    do_skip = vsketch.Param(False)
-    knot_end_mode = vsketch.Param('soft', choices=['none', 'hard', 'soft', 'continue'])
+    knot_thickness = vsketch.Param(0.15)
+    do_skip = vsketch.Param(True)
+    do_remove_circles = vsketch.Param(True)
+    knot_end_mode = vsketch.Param('connect', choices=['none', 'hard', 'soft', 'connect'])
     
     def init_tiles(self, n, detail='0.01'):
         tiles = []
@@ -56,27 +61,6 @@ class TruchetSketch(vsketch.SketchClass):
         tiles[1].arc(0, self.size, self.size, self.size, 0, np.pi/2)
         
         return tiles
-    
-    # def build_circle_tiles(self):
-    #     tiles = self.init_tiles(4)
-        
-    #     n = 5
-    #     for i in range(1, n + 1):
-    #         tiles[0].arc(0, 0, self.size, self.size, 3*np.pi/2, 2*np.pi)
-    #         tiles[0].arc(self.size, self.size, self.size, self.size, np.pi/2, np.pi)
-            
-    #         tiles[1].arc(self.size, 0, self.size, self.size, np.pi, 3*np.pi/2)
-    #         tiles[1].arc(0, self.size, self.size, self.size, 0, np.pi/2)
-            
-    #         radius = self.size * i / n
-            
-    #         tiles[2].arc(0, 0, radius, radius, 3*np.pi/2, 2*np.pi)
-    #         tiles[2].arc(self.size, self.size, radius, radius, np.pi/2, np.pi)
-            
-    #         tiles[3].arc(self.size, 0, radius, radius, np.pi, 3*np.pi/2)
-    #         tiles[3].arc(0, self.size, radius, radius, 0, np.pi/2)
-        
-    #     return tiles
     
     def build_knot_arcs(self, tiles, dx, do_skip=False, skip=0.0):
         dr = 2 * dx
@@ -108,13 +92,9 @@ class TruchetSketch(vsketch.SketchClass):
     def build_knot_tiles(self, mode):
         tiles = self.init_tiles(4)
         
-        # TODO: either only center, only outer lines or loop of lines
-        # TODO: with or without "3D"
-        # TODO: function of arcs, getting ugly
-        
-        if mode == 'only_center':
+        if mode == 'center':
             tiles = self.build_knot_arcs(tiles, 0.0)
-        elif mode == 'only_outer':
+        elif mode == 'outline':
             tiles = self.build_knot_arcs(tiles, self.knot_thickness, self.do_skip, self.knot_thickness)
             tiles = self.build_knot_arcs(tiles, -self.knot_thickness, self.do_skip, self.knot_thickness)
         elif mode == 'many':
@@ -126,39 +106,102 @@ class TruchetSketch(vsketch.SketchClass):
         
         return tiles
     
-    def add_knot_ends(self, vsk, mode='hard'):
+    def add_knot_ends(self, vsk):
         with vsk.pushMatrix():
             for y in range(self.n_y):
                 vsk.translate(0, -self.size)
-                if mode == 'hard':
+                if self.knot_end_mode == 'hard':
                     vsk.line(0, self.size / 2 - self.knot_thickness, 0, self.size / 2 + self.knot_thickness)
                     vsk.line(self.size * self.n_x, self.size / 2 - self.knot_thickness, self.size * self.n_x, self.size / 2 + self.knot_thickness)
-                elif mode == 'soft':
+                elif self.knot_end_mode == 'soft':
                     vsk.arc(0, self.size / 2, 2 * self.knot_thickness, 2 * self.knot_thickness, np.pi / 2, 3 * np.pi / 2)
                     vsk.arc(self.size * self.n_x, self.size / 2, 2 * self.knot_thickness, 2 * self.knot_thickness, -np.pi / 2, np.pi / 2)
-                elif mode == 'continue' and y % 2 == 1:
-                    vsk.arc(0, self.size, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, np.pi / 2, 3 * np.pi / 2)
-                    vsk.arc(0, self.size, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, np.pi / 2, 3 * np.pi / 2)
-                    vsk.arc(self.size * self.n_x, self.size, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, -np.pi / 2, np.pi / 2)
-                    vsk.arc(self.size * self.n_x, self.size, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, -np.pi / 2, np.pi / 2)
+                elif self.knot_end_mode == 'connect' and y % 2 == 1:
+                    if self.knot_mode == 'center':
+                            vsk.arc(0, self.size, self.size, self.size, np.pi / 2, 3 * np.pi / 2)
+                            vsk.arc(self.size * self.n_x, self.size, self.size, self.size, -np.pi / 2, np.pi / 2)
+                    elif self.knot_mode == 'outline':
+                        vsk.arc(0, self.size, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, np.pi / 2, 3 * np.pi / 2)
+                        vsk.arc(0, self.size, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, np.pi / 2, 3 * np.pi / 2)
+                        vsk.arc(self.size * self.n_x, self.size, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, -np.pi / 2, np.pi / 2)
+                        vsk.arc(self.size * self.n_x, self.size, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, -np.pi / 2, np.pi / 2)
+                    elif self.knot_mode == 'many':
+                        vsk.arc(0, self.size, self.size, self.size, np.pi / 2, 3 * np.pi / 2)
+                        vsk.arc(self.size * self.n_x, self.size, self.size, self.size, -np.pi / 2, np.pi / 2)
+                        for i in range(1, self.knot_N + 1):
+                            dr = 2 * self.knot_thickness * i / self.knot_N
+                            vsk.arc(0, self.size, self.size - dr, self.size - dr, np.pi / 2, 3 * np.pi / 2)
+                            vsk.arc(0, self.size, self.size + dr, self.size + dr, np.pi / 2, 3 * np.pi / 2)
+                            vsk.arc(self.size * self.n_x, self.size, self.size - dr, self.size - dr, -np.pi / 2, np.pi / 2)
+                            vsk.arc(self.size * self.n_x, self.size, self.size + dr, self.size + dr, -np.pi / 2, np.pi / 2)
                     
-                                    
+                                                            
         with vsk.pushMatrix():
             for x in range(self.n_x):
-                if mode == 'hard':
+                if self.knot_end_mode == 'hard':
                     vsk.line(self.size / 2 - self.knot_thickness, 0, self.size / 2 + self.knot_thickness, 0)
                     vsk.line(self.size / 2 - self.knot_thickness, -self.size * self.n_y, self.size / 2 + self.knot_thickness, -self.size * self.n_y)
-                elif mode == 'soft':
+                elif self.knot_end_mode == 'soft':
                     vsk.arc(self.size / 2, 0, 2 * self.knot_thickness, 2 * self.knot_thickness, np.pi, 0)
                     vsk.arc(self.size / 2, -self.size * self.n_y, 2 * self.knot_thickness, 2 * self.knot_thickness, 0, np.pi)
-                elif mode == 'continue' and x % 2 == 0:
-                    vsk.arc(self.size, 0, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, np.pi, 0)
-                    vsk.arc(self.size, 0, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, np.pi, 0)
-                    vsk.arc(self.size, -self.size * self.n_y, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, 0, np.pi)
-                    vsk.arc(self.size, -self.size * self.n_y, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, 0, np.pi)
-                    
+                elif self.knot_end_mode == 'connect' and x % 2 == 0:
+                    if self.knot_mode == 'center':
+                        vsk.arc(self.size, 0, self.size, self.size, np.pi, 0)
+                        vsk.arc(self.size, -self.size * self.n_y, self.size, self.size, 0, np.pi)
+                    elif self.knot_mode == 'outline':
+                        vsk.arc(self.size, 0, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, np.pi, 0)
+                        vsk.arc(self.size, 0, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, np.pi, 0)
+                        vsk.arc(self.size, -self.size * self.n_y, self.size - 2 * self.knot_thickness, self.size - 2 * self.knot_thickness, 0, np.pi)
+                        vsk.arc(self.size, -self.size * self.n_y, self.size + 2 * self.knot_thickness, self.size + 2 * self.knot_thickness, 0, np.pi)
+                    elif self.knot_mode == 'many':
+                        vsk.arc(self.size, 0, self.size, self.size, np.pi, 0)
+                        vsk.arc(self.size, -self.size * self.n_y, self.size, self.size, 0, np.pi)
+                        for i in range(1, self.knot_N + 1):
+                            dr = 2 * self.knot_thickness * i / self.knot_N
+                            vsk.arc(self.size, 0, self.size - dr, self.size - dr, np.pi, 0)
+                            vsk.arc(self.size, 0, self.size + dr, self.size + dr, np.pi, 0)
+                            vsk.arc(self.size, -self.size * self.n_y, self.size - dr, self.size - dr, 0, np.pi)
+                            vsk.arc(self.size, -self.size * self.n_y, self.size + dr, self.size + dr, 0, np.pi)
+                
                 vsk.translate(self.size, 0)
     
+    def remove_circles(self, grid):
+        has_circles = True
+        while has_circles:
+            has_circles = False
+            for x in range(self.n_x - 1):
+                for y in range(self.n_y - 1):
+                    if grid[x,y] == 0 and grid[x+1,y] == 1 and grid[x,y+1] == 1 and grid[x+1,y+1] == 0:
+                        has_circles = True
+                        for i in range(2):
+                            for j in range(2):
+                                grid[x+i,y+j] = np.random.randint(0, self.n_tiles)
+        return grid
+    
+    def remove_border_circles(self, grid):
+        has_circles = True
+        while has_circles:
+            has_circles = False
+            for x in range(self.n_x - 1):
+                if grid[x, 0] == 1 and grid[x + 1, 0] == 0:
+                    has_circles = True
+                    for i in range(2):
+                        grid[x + i, 0] = np.random.randint(0, self.n_tiles)
+                if grid[x,self.n_y - 1] == 0 and grid[x + 1, self.n_y - 1] == 1:
+                    has_circles = True
+                    for i in range(2):
+                        grid[x + i,self.n_y - 1] = np.random.randint(0, self.n_tiles)
+            for y in range(self.n_y - 1):
+                if grid[0, y] == 1 and grid[0, y + 1] == 0:
+                    has_circles = True
+                    for i in range(2):
+                        grid[0, y + i] = np.random.randint(0, self.n_tiles)
+                if grid[self.n_x - 1, y] == 0 and grid[self.n_x - 1, y + 1] == 1:
+                    has_circles = True
+                    for i in range(2):
+                        grid[self.n_x - 1, y + i] = np.random.randint(0, self.n_tiles)
+        return grid
+        
     def build_triangles_tiles(self):
         tiles = self.init_tiles(4)
         
@@ -199,24 +242,35 @@ class TruchetSketch(vsketch.SketchClass):
         vsk.detail("0.1mm")
         
         tiles = self.build_tiles(self.tile_set, self.grid)
-        n_tiles = len(tiles)
+        self.n_tiles = len(tiles)
         
         # vsk.fill(2)
         # vsk.penWidth("1mm", 2)
         # vsk.polygon([(0,0), (self.size,0), (self.size, self.size)], close=True)
         
+        # TODO: first make grid of ints, then refine (remove circles), then draw
+        index_grid = np.zeros((self.n_x, self.n_y), dtype=int)
+        for y in range(self.n_y):
+            for x in range(self.n_x):
+                tile_index = np.random.randint(0, self.n_tiles)
+                index_grid[x,y] = tile_index
+        
+        if self.tile_set == self.tile_sets.knot.name and self.do_remove_circles:
+            index_grid = self.remove_circles(index_grid)
+            if self.knot_end_mode == 'connect':
+                index_grid = self.remove_border_circles(index_grid)
+            
         # Draw grid of random tiles from chosen tile set:
         for y in range(self.n_y):
             with vsk.pushMatrix():
                 for x in range(self.n_x):
-                    tile_index = np.random.randint(0, n_tiles)
-                    vsk.sketch(tiles[tile_index])
+                    vsk.sketch(tiles[index_grid[x,y]])
                     vsk.translate(self.size, 0)
             vsk.translate(0, self.size)
         
         if self.tile_set == self.tile_sets.knot.name and self.knot_end_mode != 'none':
-            self.add_knot_ends(vsk, self.knot_end_mode)
-            
+            self.add_knot_ends(vsk)
+                
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
 
