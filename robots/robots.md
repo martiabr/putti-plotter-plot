a278ef3914e96d1cc2594ae4af602141fe4272f4
martiabr <martin.abrandt@outlook.com>
2022-06-13 19:28:56 +0200

    Add more eyes and mouths

---
+++ robots/robots.md
@@ -0,0 +1,8 @@
+### TODO parts:
+- Rounded grill mouth
+
+### TODO randomize:
+- Arm placement
+- Leg placement
+- Eye placement
+- Mouth placement
--- robots/sketch_robots.py
+++ robots/sketch_robots.py
@@ -3,18 +3,12 @@ import numpy as np
 from enum import Enum
 from curve_utils import draw_bezier, draw_line_thick, draw_bezier_thick
 
-# Plan:
-# - Move bezier code to seperate file
-# - Good maintainable code structure
-# - Make helper functions and extract good stuff from rocket
-# - Start experimenting
-
 # - Should work such that I can call a series of functions and then draw any robot I want. 
 #   Allows to create a drawing of every type possible, e.g. show every possible mouth, eyes, arms...
 #   To have an overview.
 # - Maybe have two scripts - robot editor, and random robot grid generator 
 
-        
+
 def test_bezier(vsk):
     x_start = np.array([0.0, 0.0])
     x_c1 = np.array([4.0, 0.0])
@@ -50,6 +44,27 @@ def pick_random_element(elements, probs):
 
 def enum_type_to_int(enum_type):
     return enum_type.value - 1
+
+def generate_point_eye_sketch(detail="0.01"):
+    eye_sketch = vsketch.Vsketch()
+    eye_sketch.detail(detail)
+    eye_sketch.circle(0, 0, 1e-2)
+    return eye_sketch
+
+def generate_ellipse_point_eye_sketch(detail="0.01"):
+    eye_sketch = vsketch.Vsketch()
+    eye_sketch.fill(1)
+    eye_sketch.detail(detail)
+    eye_sketch.ellipse(0, 0, 0.05, 0.075)
+    eye_sketch.noFill()
+    return eye_sketch
+
+def generate_circle_eye_sketch(radius, x_pupil_gain, detail="0.01"):
+    eye_sketch = vsketch.Vsketch()
+    eye_sketch.detail(detail)
+    eye_sketch.circle(0, 0, radius=radius)
+    eye_sketch.circle(x_pupil_gain * radius, 0, 1e-2)
+    return eye_sketch
 
 def generate_arm_sketch(x_end, y_end, x_c1, y_c1, x_c2, y_c2, width, width_end=None,
                         shoulder_width=None, shoulder_height=None, use_shoulder=False,
@@ -103,6 +118,13 @@ def draw_bullet_body(vsk, width, arc_height, lower_height):
     bullet_body_shape.arc(0, 0, width, arc_height, 0, 1.01*np.pi, close="chord", mode="center")  # Note: 1.01 factor is there for shapes to connect
     bullet_body_shape.rect(0, 0.5 * lower_height, width, lower_height, mode="center")
     vsk.shape(bullet_body_shape)
+    
+    
+### Mouths:
+
+def draw_grill_mouth(vsk, width, height, N_lines, debug=False):
+    draw_line_thick(vsk, np.array([-0.5 * width, 0]), np.array([0.5 * width, 0]), height, N_lines=N_lines, debug=debug)
+
 
 class RobotsSketch(vsketch.SketchClass):
     
@@ -131,18 +153,43 @@ class RobotsSketch(vsketch.SketchClass):
     body_rect_inner_padding_max = vsketch.Param(0.75)
     body_rect_inner_padding_min = vsketch.Param(0.1)
     
-    body_circle_radius_max = vsketch.Param(2.0, min_value=0)
-    body_circle_radius_min = vsketch.Param(1.0, min_value=0)
+    body_circle_radius_max = vsketch.Param(2.5, min_value=0)
+    body_circle_radius_min = vsketch.Param(1.2, min_value=0)
     
     inner_circle_prob = vsketch.Param(0.3, min_value=0, max_value=1)
     body_circle_inner_padding_max = vsketch.Param(0.5)
     body_circle_inner_padding_min = vsketch.Param(0.1)
     
-    body_bullet_radius_max = vsketch.Param(2.0, min_value=0)
-    body_bullet_radius_min = vsketch.Param(1.0, min_value=0)
+    body_bullet_radius_max = vsketch.Param(3.0, min_value=0)
+    body_bullet_radius_min = vsketch.Param(0.8, min_value=0)
     body_bullet_lower_height_max = vsketch.Param(2.0, min_value=0)
-    body_bullet_lower_height_min = vsketch.Param(1.0, min_value=0)
+    body_bullet_lower_height_min = vsketch.Param(0.5, min_value=0)
     
+    # Eye parameters:
+    eye_types = Enum('EyeType', 'POINT ELLIPSE_POINT CIRCLE')
+    eye_point_prob = vsketch.Param(0.25, min_value=0)
+    eye_ellipse_point_prob = vsketch.Param(0.25, min_value=0)
+    eye_circle_prob = vsketch.Param(0.5, min_value=0)
+    
+    eye_circle_radius_gain_max = vsketch.Param(0.1, min_value=0)
+    eye_circle_x_pupil_gain_max = vsketch.Param(0.2, min_value=0)
+    eye_circle_radius_gain_min = vsketch.Param(0.01, min_value=0)
+    
+    eye_x_gain_max = vsketch.Param(0.6, min_value=0)
+    eye_x_gain_min = vsketch.Param(0.1, min_value=0)
+    
+    # Mouth parameters:
+    mouth_types = Enum('MouthType', 'NONE SMILE GRILL')
+    mouth_none_prob = vsketch.Param(0.5, min_value=0)
+    mouth_smile_prob = vsketch.Param(0.25, min_value=0)
+    mouth_grill_prob = vsketch.Param(0.25, min_value=0)
+    
+    mouth_grill_width_gain_max = vsketch.Param(0.8, min_value=0)
+    mouth_grill_width_gain_min = vsketch.Param(0.3, min_value=0)
+    mouth_grill_height_max = vsketch.Param(0.6, min_value=0)
+    mouth_grill_height_min = vsketch.Param(0.075, min_value=0)
+    mouth_grill_N_lines_max = vsketch.Param(8, min_value=0)
+    mouth_grill_N_lines_min = vsketch.Param(3, min_value=0)
     
     # Arm parameters:
     arm_shoulder_prob = vsketch.Param(0.5, min_value=0, max_value=1)
@@ -161,6 +208,7 @@ class RobotsSketch(vsketch.SketchClass):
             self.body_width = np.random.uniform(self.body_rect_width_min, self.body_rect_width_max)
             self.body_height = np.random.uniform(self.body_rect_height_min, self.body_rect_height_max)
             self.body_lower_height = 0.5 * self.body_height
+            self.body_upper_height = 0.5 * self.body_height
             
             use_inner_rect = np.random.random_sample() < self.inner_rect_prob
             if use_inner_rect:
@@ -173,6 +221,7 @@ class RobotsSketch(vsketch.SketchClass):
             self.body_width = np.random.uniform(self.body_circle_radius_min, self.body_circle_radius_max)
             self.body_height = self.body_width
             self.body_lower_height = 0.5 * self.body_height
+            self.body_upper_height = 0.5 * self.body_height
             
             use_inner_circle = np.random.random_sample() < self.inner_circle_prob
             if use_inner_circle:
@@ -184,25 +233,45 @@ class RobotsSketch(vsketch.SketchClass):
         elif body_choice == enum_type_to_int(self.body_types.BULLET):
             self.body_width = np.random.uniform(self.body_bullet_radius_min, self.body_bullet_radius_max)
             arc_height = np.random.uniform(self.body_bullet_radius_min, self.body_bullet_radius_max)
-            lower_height = np.random.uniform(self.body_bullet_lower_height_min, self.body_bullet_lower_height_max)
-            self.body_lower_height = lower_height
+            self.body_lower_height = np.random.uniform(self.body_bullet_lower_height_min, self.body_bullet_lower_height_max)
+            self.body_upper_height = arc_height
             
-            draw_bullet_body(vsk, self.body_width, arc_height, lower_height)
+            draw_bullet_body(vsk, self.body_width, arc_height, self.body_lower_height)
             
         # Eyes:
-        vsk.fill(1)
+        eye_choice = pick_random_element(self.eye_types, self.eye_type_probs)
+        
+        if eye_choice == enum_type_to_int(self.eye_types.POINT):
+            eye_sketch = generate_point_eye_sketch()
+        elif eye_choice == enum_type_to_int(self.eye_types.ELLIPSE_POINT):
+            eye_sketch = generate_ellipse_point_eye_sketch()
+        elif eye_choice == enum_type_to_int(self.eye_types.CIRCLE):
+            eye_radius = np.random.uniform(self.eye_circle_radius_gain_min, self.eye_circle_radius_gain_max) * self.body_width
+            pupil_x_gain = np.random.uniform(-self.eye_circle_x_pupil_gain_max, self.eye_circle_x_pupil_gain_max)
+            eye_sketch = generate_circle_eye_sketch(eye_radius, pupil_x_gain)
+            
+        eye_x_gain = np.random.uniform(self.eye_x_gain_min, self.eye_x_gain_max)
         with vsk.pushMatrix():
-            vsk.translate(0.2 * self.body_width, 0)    
-            vsk.ellipse(0, 0, 0.05, 0.075)
-        with vsk.pushMatrix():
-            vsk.translate(-0.2 * self.body_width, 0)    
-            vsk.ellipse(0, 0, 0.05, 0.075)
-        vsk.noFill()
+            vsk.translate(eye_x_gain * 0.5 * self.body_width, 0)
+            vsk.sketch(eye_sketch)
+            vsk.translate(-eye_x_gain * self.body_width, 0)
+            if np.random.random_sample() < 0.5: vsk.scale(-1, 1)
+            vsk.sketch(eye_sketch)
+        
+        
         
         # Mouth:
+        mouth_choice = pick_random_element(self.mouth_types, self.mouth_type_probs)
         with vsk.pushMatrix():
-            vsk.translate(0, 0.2* self.body_lower_height)    
-            draw_bezier(vsk, np.array([-0.1, 0]), np.array([0.1, 0]), np.array([-0.1, 0.2]), np.array([0.1, 0.2]), debug=debug)
+            vsk.translate(0, 0.2 * self.body_lower_height)
+            if mouth_choice == enum_type_to_int(self.mouth_types.SMILE):
+                draw_bezier(vsk, np.array([-0.1, 0]), np.array([0.1, 0]), np.array([-0.1, 0.2]), np.array([0.1, 0.2]), debug=debug)  # TODO: function of this
+            elif mouth_choice == enum_type_to_int(self.mouth_types.GRILL):
+                mouth_width = np.random.uniform(self.mouth_grill_width_gain_min, self.mouth_grill_width_gain_max) * self.body_width
+                mouth_height = np.random.uniform(self.mouth_grill_height_min, self.mouth_grill_height_max)
+                mouth_N_lines = np.random.randint(self.mouth_grill_N_lines_min, self.mouth_grill_N_lines_max + 1)
+                vsk.translate(0, 0.5 * mouth_height)
+                draw_grill_mouth(vsk, mouth_width, mouth_height, mouth_N_lines, debug=debug)
 
     def draw_robot(self, vsk, debug=False):
         self.draw_body(vsk, debug)
@@ -239,9 +308,9 @@ class RobotsSketch(vsketch.SketchClass):
         # Draw antennas:
         # antenna_sketch = generate_antenna_sketch(0.25, 0.15, 0.075, 0.6, 0.2)
         # with vsk.pushMatrix():
-        #     vsk.translate(0.3 * body_width, -0.5 * body_height - 0.5 * 0.15)
+        #     vsk.translate(0.3 * self.body_width, -self.body_upper_height - 0.5 * 0.15)
         #     vsk.sketch(antenna_sketch)
-        #     vsk.translate(-2 * 0.3 * body_width, 0)
+        #     vsk.translate(-2 * 0.3 * self.body_width, 0)
         #     vsk.sketch(antenna_sketch)
         
     
@@ -251,6 +320,8 @@ class RobotsSketch(vsketch.SketchClass):
         vsk.scale(self.scale, self.scale)
         
         self.body_type_probs = np.array([self.body_rect_prob, self.body_circle_prob, self.body_bullet_prob])
+        self.mouth_type_probs = np.array([self.mouth_none_prob, self.mouth_smile_prob, self.mouth_grill_prob])
+        self.eye_type_probs = np.array([self.eye_point_prob, self.eye_ellipse_point_prob, self.eye_circle_prob])
         
         # test_bezier(vsk)
         # test_line_thick(vsk)
