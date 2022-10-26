7d7410480cee461bd73df1aecbaf6d4c0fc5ca91
Martin Albertsen Brandt <martin.brandt@sintef.no>
2022-10-26 13:04:06 +0200

    Update exoplanet dataset

--- exo/sketch_exo.py
+++ exo/sketch_exo.py
@@ -7,7 +7,7 @@ class ExoSketch(vsketch.SketchClass):
     # radius = vsketch.Param(2.0)
     
     def get_col_names(self, file_path):
-        col_names = pd.read_csv(file_path, names=["Abbrv", "Column name"], delimiter=":", skiprows=5, nrows=77)
+        col_names = pd.read_csv(file_path, names=["Abbrv", "Column name"], delimiter=":", skiprows=5, nrows=75)
         col_names["Abbrv"] = col_names["Abbrv"].str.replace("# COLUMN ", "")
         col_names["Abbrv"] = col_names["Abbrv"].str.strip()
         col_names["Column name"] = col_names["Column name"].str.strip()
@@ -15,10 +15,9 @@ class ExoSketch(vsketch.SketchClass):
     
     def process_data(self, file_path, cols_filter=["pl_orbincl", "pl_orbper"], min_num_planets=3):
         col_names = self.get_col_names(file_path)
-        # print(col_names)
         
-        df = pd.read_csv(file_path, header=83, nrows=10000)
-        df["rowupdate"] = pd.to_datetime(df["rowupdate"])
+        df = pd.read_csv(file_path, header=81)
+        # df["rowupdate"] = pd.to_datetime(df["rowupdate"])
         
         # TODO:
         # - Remove doubles (keep most recent?)
@@ -31,6 +30,10 @@ class ExoSketch(vsketch.SketchClass):
         # - Filter away items with too few planets
         # - Filter away not confirmed?
         # - Create plots of the data! E.g. histograms
+        # - Filter away star systems with too few planets after filtering away NaNs
+        # - Add dict manually for doing GJ->Gliese, Cnc-> Cancri, KOI->...->Kepler to get nicer names to print
+        # - Get data on stars as well? Especially for binary sytems...
+        # - Get composition data - draw plots?
         
         # def col_name_to_abbrv(col_name):
         #     return col_names[col_names["Column name"] == col_name]["Abbrv"]
@@ -40,17 +43,17 @@ class ExoSketch(vsketch.SketchClass):
         
         # print((df[df["soltype"] != "Published Confirmed"]))
         
-        df = df[df[cols_filter].notnull().all(1)]
-        
-        print(df)
+        df = df[df[cols_filter].notnull().all(1)].reset_index(drop=True)
         
         # Remove duplicates:
         # df = df.groupby("pl_name").mean().reset_index()  # merge using mean
-        df = df.loc[df.groupby("pl_name").rowupdate.idxmax()]  # pick newest
+        # df = df.loc[df.groupby("pl_name").rowupdate.idxmax()].reset_index(drop=True)  # pick newest
         
         print(df)
         
         print(df.groupby("hostname"))
+        
+        df.to_csv("exo_test.csv")
         
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
@@ -60,7 +63,7 @@ class ExoSketch(vsketch.SketchClass):
         # implement your sketch here
         # vsk.circle(0, 0, self.radius, mode="radius")
         
-        self.process_data("data/PS_2022.10.24_10.45.43.csv")
+        self.process_data("data/PSCompPars_2022.10.26_03.59.46.csv")
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
